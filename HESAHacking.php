6c31372acde99231019af135cb3e2beee5c14242
Christopher Gutteridge <totl@soton.ac.uk>
2017-10-10 14:46:53 +0100

    init

---
+++ HESAHacking.php
@@ -0,0 +1,60 @@
+<?php
+$base_dir = __DIR__;
+
+$source_dir = "$base_dir/source_data";
+require_once( "$base_dir/arc/ARC2.php" );
+require_once( "$base_dir/Graphite/Graphite.php" );
+
+# read a csv file with optional assigned column headings
+# get otherwise use first row for headings
+function readCSV( $filename, $fields=null ) {
+	return readRows( $filename, $fields, "str_getcsv" );
+}
+
+function _splitOnTab( $string ) {
+	return preg_split( "/\t/", $string );
+}
+
+# read a tsv file with optional assigned column headings
+# get otherwise use first row for headings
+function readTSV( $filename, $fields=null ) {
+	return readRows( $filename, $fields, "_splitOnTab" );
+}
+
+function readRows( $filename, $fields, $splitFn ) {
+	$lines = file($filename);
+	if( $fields === null ) {
+		$fields = $splitFn( chop( array_shift( $lines ) ));
+	}
+
+	$records = array();
+	foreach( $lines as $line ) {
+		$cells = $splitFn( chop( $line ) );
+		$record = array();
+		for( $i=0; $i<count($fields); ++$i ) {
+			$value = trim( $cells[$i] );
+			if( $value == "#N/A" ) { $value = null; }
+			$record[$fields[$i]] = $value;
+		}
+		$records []= $record;
+	}
+	return $records;
+}
+
+##################################################################
+# Data field functions
+##################################################################
+
+# This function tries to deal with sloppy URLS missing the protocol.
+function cleanURL( $url ) {
+
+	if( !preg_match( "/^[a-z]+:/", $url ) ) { 
+		$url = "http://$url";
+	}
+	return $url;
+}
+
+function stripWhitespace( $text ) {
+	return preg_replace( "/\s+/","", $text );
+}
+
